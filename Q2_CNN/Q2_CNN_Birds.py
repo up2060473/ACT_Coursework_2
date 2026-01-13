79563af817d9b023ac7cb5b7f1e15a5ada016b7a
up2060473 <up2060473@myport.ac.uk>
2026-01-13 11:07:34 -0500

    Q2: Added loading of data, initial transforms (will look into augmentation later) and investigated class distribution

--- Q2_CNN/Q2_CNN_Birds.py
+++ Q2_CNN/Q2_CNN_Birds.py
@@ -1,7 +1,7 @@
 data_path = "../../../Data_birds/birds_dataset/Bird Speciees Dataset/"
 ## Currently, this is accessing local file on sciserver
 ## To be updated to take dataset from Kaggle
-
+print("\n>>> Starting ")
 ## Importing Libraries 
 ## To be updated to be store in a dependancies folder
 ## Torch
@@ -19,13 +19,47 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 ## Which device?
-print("##### Starting #####")
-print("GPU Available: ", torch.cuda.is_available())
+print("\n>>> Device\nGPU Available: ", torch.cuda.is_available())
 
 ## Setting Device
 if torch.cuda.is_available() == False:
-    print("Switching to CPU.... \nMay result in slower performance")
+    print("Switching to CPU.... \nMay result in slower performance\n\n")
     device = "cpu"
 else:
-    print("Cuda GPU found, switching device to increase performance")
+    print("Cuda GPU found, switching device to increase performance\n\n")
     device = "cuda"
+
+## Transforms
+transform = transforms.Compose([
+    transforms.Resize((128,128)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])    
+
+## Loading dataset and defining batch size
+data = torchvision.datasets.ImageFolder(
+    root = data_path,
+    transform = transform,
+)
+batch_size = 16
+
+## Investigating the distribution of samples per class in the dataset
+#print(dict(Counter(data.targets)))
+#print(data.class_to_idx)
+
+#counter = dict(Counter(data.targets))
+#index = dict(data.class_to_idx)
+#counts = dict({tuple(index.keys()):tuple(counter.values())})
+#print(counts)
+
+#class_counts = Counter(label for _, label in data)
+#for class_idx, count in class_counts.items():
+#    class_name = data.classes[class_idx]
+#    print(class_name, " : ", count, " samples")
+
+## Investigation distribution
+print(">>> Investigating distribution")
+class_counts = Counter(data.targets)
+for index, count in class_counts.items():
+    print(f"{data.classes[index]} : {count}")
+    
