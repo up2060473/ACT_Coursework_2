a04a5db15ce711bb27f66ce5bab94b801e5e88f8
up2060473 <up2060473@myport.ac.uk>
2026-01-13 14:30:17 -0500

    Q2: Defined CNN, currently at 80% training at 15 epochs

--- Q2_CNN/Q2_CNN_Birds.py
+++ Q2_CNN/Q2_CNN_Birds.py
@@ -58,6 +58,7 @@ batch_size = 16
 #    print(class_name, " : ", count, " samples")
 
 ## Investigation distribution
+classes = data.classes
 print(">>> Investigating distribution")
 class_counts = Counter(data.targets)
 for index, count in class_counts.items():
@@ -71,7 +72,6 @@ test_size = len(data) - train_size
 train_dataset, test_dataset = random_split(data, [train_size, test_size])
 
 ## Data_loaders
-## num_workers = 0 as 2 returned error - sciserver
 train_loader = DataLoader(
     train_dataset,
     batch_size = batch_size,
@@ -92,10 +92,98 @@ def imshow(img):
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1,2,0)))
     plt.axis("off")
-    plt.title("Example Images")
+    plt.title("Example images from the training dataset")
     plt.show()
 dataiter = iter(train_loader)
 images, labels = next(dataiter)
 imshow(torchvision.utils.make_grid(images))
-plt.savefig("images.png")
+plt.savefig("example_training_images.png")
 
+## Defining a CNN - Inspiried by class notes
+print("\n\n>>> Defining a CNN")
+class Net(nn.Module):
+    def __init__(self, num_classes = 6, input_size=128):
+        super().__init__()
+        ## Conv layers
+        self.conv1 = nn.Conv2d(3,6,5)
+        self.pool = nn.MaxPool2d(2,2)
+        self.conv2 = nn.Conv2d(6,16,5)
+        
+        ## FN layers
+        self.fc1 = nn.Linear(16*29*29, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, num_classes)
+    def forward(self, x):
+        ## conv layers, relu and pooling
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        ## Flattern
+        x = torch.flatten(x, 1)
+        ## FC layer, relu
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        ## Output later
+        x = self.fc3(x)
+        return x
+##Create network
+net = Net(num_classes=6)
+print(net)
+
+##Defining loss function and optimiser
+## Adam optimiser - originally tried with SGD but not as efficient or accurate
+print("\n\n>>> Defining Cross Entropy Loss and Adam optimiser (learning rate = 0.001)")
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(net.parameters(), lr=0.001)
+net.to(device)
+
+## Training the network
+## Training for 15 epochs
+print("\n\n>>> Training network, 15 batches")
+for epoch in range(15):
+    running_loss = 0.0
+    
+    for i, (inputs, labels) in enumerate(train_loader):
+        inputs, labels = inputs.to(device), labels.to(device)
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+        
+        ## Print statistics every 10 batches
+        if (i+1) % 10 ==0:
+            print(f"Epoch {epoch+1}, Batch {i+1} Loss: {running_loss / 10:.3f}")
+            running_loss = 0.0
+print("\n\n>>> Finished Training")
+
+## Save the model
+PATH = "./bird_classifier.pth"
+torch.save(net.state_dict(), PATH)
+print("\n\n>>> Saved model to ./bird_classifier.pth")
+
+
+## Initialising correct prediction and total prediction dictionaries
+correct_pred = {classname: 0 for classname in classes}
+total_pred = {classname: 0 for classname in classes}
+
+## Testing model and saving predictions 
+print(">>> Testing model")
+with torch.no_grad():
+    for data in test_loader:
+        images, labels = data
+        images, labels = images.to(device), labels.to(device)
+        outputs = net(images)
+        _, predictions = torch.max(outputs, 1)
+        
+        for label, prediction in zip(labels, predictions):
+            label = label.item()
+            prediction = prediction.item()
+            if label == prediction:
+                correct_pred[classes[label]] += 1
+            total_pred[classes[label]] += 1
+# print accuracy for each class
+for classname, correct_count in correct_pred.items():
+    accuracy = 100 * float(correct_count) / total_pred[classname]
+    print("\n\n>>> Testing completed, results follow")
+    print(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
