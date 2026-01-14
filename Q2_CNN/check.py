3c0678e129be19a40ccff126c5835834179e65fb
up2060473 <up2060473@myport.ac.uk>
2026-01-14 18:10:38 -0500

    Final commit before closure of branch

---
+++ Q2_CNN/check.py
@@ -0,0 +1,9 @@
+import numpy
+import scipy
+import pandas
+import bqplot
+
+print("numpy:", numpy.__version__)
+print("scipy:", scipy.__version__)
+print("pandas:", pandas.__version__)
+print("bqplot:", bqplot.__version__)
