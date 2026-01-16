8e5ed31d94e2d7525d1548a1b13d70256b259a78
up2060473 <up2060473@myport.ac.uk>
2026-01-16 02:15:18 -0500

    Added two dependancy folders - seperate now to stop the error carry forward for open cv (failsafe)

---
+++ Archive/Check_dependancies.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib
+import seaborn as sns
+import numpy as np
+import cv2
+import sklearn
+import skimage
+import torch
+import torchvision
+
+print("pandas:", pd.__version__)
+print("matplotlib:", matplotlib.__version__)
+print("seaborn:", sns.__version__)
+print("numpy:", np.__version__)
+print("cv2:", cv2.__version__)
+print("scikit-learn:", sklearn.__version__)
+import skimage
+print("scikit-image:", skimage.__version__)
+print("torch:", torch.__version__)
+print("torchvision:", torchvision.__version__)
+
